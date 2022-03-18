56643348dadc1fdcc8efc9c879d1393cd63d9554
Maksemqwe123 <101874489+Maksemqwe123@users.noreply.github.com>
2022-03-18 17:07:59 +0300

    lesson 2

--- thory.py
+++ thory.py
@@ -5,3 +5,50 @@
 
 '''Ввод и вывод строки'''
 
+ # nums = 123
+# str_nums = str(nums)
+#
+# print(type(nums))
+# print(type(str_nums))
+# go = "тут что-то не так,не будь я д'Артаньян\" - подумал он."
+# print(go)
+
+# a= "тут что-то не так,не будь я д'Артаньян\" - подумал он."
+# z = "пока"
+# b = a[3:8]
+#
+# print(len(a))
+# -
+# string = 'Helloooo'
+# print(string[::3])na
+# name = input("Введите ваше имя ")
+# age = int(input("Введите ваш возраст"))
+# weight = int(input("Введите ваш вес"))
+# print(f'Заика сказал:{name * 2}')
+# print(f'Возрас в секундах:{age * 365 * 24 * 60 * 60} ')
+# print(f'вес на луне:{weight // 6}')
+
+# number = 0
+# if number > 0:
+#     print("число больше нуля")
+#     if number % 2 == 0:
+#         print("число чётное")
+#     else:
+#         print("число не чётное")
+# elif number < 0:
+#     print("число меньше нуля")
+#     if number % 2 == 0:
+#         print("число чётное")
+#     else:
+#         print("число не чётное")
+# else:
+#     print("число равно нулю")
+# print("конец проги")
+
+# health = 25
+#
+# if  health <= 0:
+#     print("персанаж умер")
+# else:
+#     print("персонаж жив")
+#
