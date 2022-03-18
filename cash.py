8c8663a955fb31b5bd88e3c14dd844db091256c5
Maksemqwe123 <101874489+Maksemqwe123@users.noreply.github.com>
2022-03-18 15:22:05 +0300

    lesson 1

---
+++ cash.py
@@ -0,0 +1,13 @@
+"""программа подсчет финансо"""
+food = int(input ("Введите расходы на еду в рублях"))
+internet = int(input("Введите расходы на интернет в рублях"))
+
+
+# print(type(food))
+# print(food)
+# print(type(internet))
+# print(internet)
+# total_cash = food + internet
+# print(total_cash)
+total_cash = food + internet
+print(f"общие расходы :{total_cash}")
---
+++ thory.py
@@ -0,0 +1,7 @@
+# first_name = input("Введите ваше имя ")
+# last_name = input("Введите вашу фамилию ")
+#
+# print(f"{first_name} \n {last_name}")
+
+'''Ввод и вывод строки'''
+
